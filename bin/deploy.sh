5099d2e37c5a4c4b6536c0d8f202c6d9fee8ddc9
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2020-09-22 12:48:01 -0400

    css cleanup

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,3 @@
-$BUILD_DIR='public'
-rm -rf $BUILD_DIR
+rm -rf './public/*'
 hugo
 rsync --omit-dir-times -r -a -v -e ssh --delete './public/' geoservices@$GEOSERVICES_IP:/var/www/geoservices/main-site-redesign
